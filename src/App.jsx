7486ed60a199660a80bca59ff72949fe5b10f376
Pranav2302 <21118023@pvgcoet.ac.in>
2025-04-04 16:42:50 +0530

    setup of vite , tailwind , shadcn , one example of acternity components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { LampDemo } from "@/components/ui/lamp"  
+function App() {
+
+
+  return (
+    
+     <LampDemo/>
+
+  )
+}
+
+export default App
