f75429d485604fc0964f1ae0f3165c2c66192225
Pranav2302 <21118023@pvgcoet.ac.in>
2025-04-10 18:47:20 +0530

    unnecessary files deleted

--- src/pages/Certification.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Certification() {
-  return (
-    <div className="bg-black py-20 w-full">
-      <h1 className="text-4xl font-bold text-white text-center">Certification Page</h1>
-    </div>
-  )
-}
--- src/pages/Products.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Products() {
-  return (
-    <div className="bg-black py-20 w-full">
-      <h1 className="text-4xl font-bold text-white text-center">Products Page</h1>
-    </div>
-  )
-}
